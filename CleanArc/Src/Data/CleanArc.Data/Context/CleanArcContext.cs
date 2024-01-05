db711e0552301e4ac4182a18d74dcc1b9e33faf0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-01-05 12:56:21 +0330

    create project and create data layer

---
+++ CleanArc/Src/Data/CleanArc.Data/Context/CleanArcContext.cs
@@ -0,0 +1,10 @@
+﻿using CleanArc.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArc.Data;
+
+public class CleanArcContext(DbContextOptions<CleanArcContext> options) : DbContext(options)
+{
+
+    public virtual DbSet<Course> Course { get; set; }
+}
